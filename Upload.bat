bc6a6cdbc7e3d57405f31b368eb3a255a5804021
caxmirror <627085892@qq.com>
2023-02-17 01:09:23 +0800

    CIFR10Recognition 1.0

---
+++ Upload.bat
@@ -0,0 +1,8 @@
+git init
+git remote add origin https://github.com/caxmirror/CIFR10Recognition.git
+git config user.name "caxmirror"
+git config user.email "627085892@qq.com"
+git add .
+git commit -m "CIFR10Recognition 1.0"
+git push -u origin master
+pause
